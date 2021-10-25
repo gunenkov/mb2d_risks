9b3daaefb5a19d16c9c9587a1b6323917db08b13
Vakil Gayfullin <g.vakil@yandex.ru>
2021-10-25 14:06:43 +0300

    Добавил auth, dashboard

--- Frontend/risks/src/app/app-routing.module.ts
+++ Frontend/risks/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './pages/auth/auth.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'auth', component: AuthComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: '**', redirectTo: '/'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Frontend/risks/src/app/app.module.ts
+++ Frontend/risks/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './pages/auth/auth.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
