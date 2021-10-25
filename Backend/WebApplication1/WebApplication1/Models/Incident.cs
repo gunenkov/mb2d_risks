5a94a0f5dc1c7d78808235fcbb4f146adc765169
Mikhail Gunenkov <m.gunenkov@gmail.com>
2021-10-25 14:39:30 +0300

    Updated database

--- Backend/WebApplication1/WebApplication1/Models/Incident.cs
+++ Backend/WebApplication1/WebApplication1/Models/Incident.cs
@@ -8,5 +8,7 @@ namespace WebApplication1.Models
         public string Name {  get; set; }
         public int RiskId {  get; set; }
         public DateTime DateTime {  get; set; }
+        public bool Ccorresponds { get; set; }
+        public IncidentResult Result { get; set; }
     }
 }
---
+++ Backend/WebApplication1/WebApplication1/Models/IncidentResult.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models
+{
+    public enum IncidentResult
+    {
+        Escaped,
+        Minimized,
+        Passed,
+        Accepted
+    }
+}
